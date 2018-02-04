c972e54ea09e97ce579ff84945d6a0f353ec22f1
Gregory Smith, II <gws_ii@msn.com>
2018-02-03 16:36:27 -0800

    * Fix for localhost build * break up build and deploy npm commands

--- build.js
+++ build.js
@@ -2,45 +2,38 @@ const cp = require('child_process')
 const fs = require('fs')
 const path = require('path')
 
-const stage = process.argv[2]
+const stage = process.argv[2] || '.'
 
-if (!stage) {
-  console.error('Please provide a stage name as the first argument.')
-  process.exit(1)
-}
-
-if (!stage.match(/[a-zA-Z][a-zA-Z0-9/-]{0,99}/)) {
+if (!stage.match(/(\.|[a-zA-Z][a-zA-Z0-9/-]{0,99})/)) {
   console.error([
     'The stage name should only contain alphanumeric (case sensitive) and hyphens.',
     'It should start with an alphabetic character and shouldn\'t exceed 100 characters.',
-  ].join(''))
-  process.exit(2)
+  ].join('\n'))
+  process.exit(1)
 }
 
-// Init & Read package.json
+// Init & read package.json
 const reactAppPackagePath = path.join(__dirname, 'browser-client', 'package.json')
-const reactAppPackageBackup = reactAppPackagePath + '.bak'
 const reactAppPackage = fs.readFileSync(reactAppPackagePath, 'utf8')
 const package = JSON.parse(reactAppPackage)
 
-// Backup & Replace with updated package
+// Backup & replace with updated package.json, where homepage: stage
 const buildPackage = Object.assign({ homepage: `/${stage}/`}, package)
+const reactAppPackageBackup = reactAppPackagePath + '.bak'
 fs.writeFileSync(reactAppPackageBackup, reactAppPackage)
 fs.writeFileSync(reactAppPackagePath, JSON.stringify(buildPackage))
 
-// Run the app build
-console.log(`Building for STAGE "${stage}"...`)
-cp.execSync('npm run build', {
-  cwd: path.join(__dirname, 'browser-client'),
-  stdio: [0, 1, 2],
-})
-
-// Restore package
-fs.writeFileSync(reactAppPackagePath, reactAppPackage)
-fs.unlinkSync(reactAppPackageBackup)
-
-// Deploy via Serverless
-console.log(`Deploying STAGE "${stage}"...`)
-cp.execSync(`sls deploy --stage ${stage}`, { stdio: [0, 1, 2] })
+try {
+  // Run the app build
+  console.log(`Building for ${stage === '.' ? 'localhost' : ` STAGE "${stage}"`}...`)
+  cp.execSync('npm run build', {
+    cwd: path.join(__dirname, 'browser-client'),
+    stdio: [0, 1, 2],
+  })
+} finally {
+  // Restore package
+  fs.writeFileSync(reactAppPackagePath, reactAppPackage)
+  fs.unlinkSync(reactAppPackageBackup)
+}
 
 console.log('Done.')
